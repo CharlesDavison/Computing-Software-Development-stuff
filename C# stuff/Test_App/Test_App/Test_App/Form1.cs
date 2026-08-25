24b54cd2928d4a0308b2a7cc3e839ad396b32b29
CharlesDavison <1927458@fife.ac.uk>
2026-08-25 10:13:47 +0100

    :)

--- C# stuff/Test_App/Test_App/Test_App/Form1.cs
+++ C# stuff/Test_App/Test_App/Test_App/Form1.cs
@@ -2,6 +2,9 @@ namespace Test_App
 {
     public partial class Form1 : Form
     {
+
+        String Button3Text = "Change This Text";
+
         public Form1()
         {
             InitializeComponent();
@@ -15,6 +18,19 @@ namespace Test_App
         private void button2_Click(object sender, EventArgs e)
         {
             Environment.Exit(0);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (Button3Text == "Change This Text")
+            {
+                Button3Text = "Text Has been changed";
+                button3.Text = Button3Text;
+            } else
+            {
+                Button3Text = "Change This Text";
+                button3.Text = Button3Text;
+            }
         }
     }
 }
--- C# stuff/Test_App/Test_App/Test_App/obj/Debug/net8.0-windows/Test_App.AssemblyInfo.cs
+++ C# stuff/Test_App/Test_App/Test_App/obj/Debug/net8.0-windows/Test_App.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Test_App")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dce37d68c1cae4e5a42b7c060c616bb06bcd5c15")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b64794d43ca65ca463467830ec5671c5ea7cc127")]
 [assembly: System.Reflection.AssemblyProductAttribute("Test_App")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Test_App")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
